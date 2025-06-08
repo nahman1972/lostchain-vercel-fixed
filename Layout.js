518c4adc7e47896b016a6615c3a0251132914cb6
nahman1972 <hristov.dumitru@gmail.com>
2025-06-08 03:18:15 -0400

    Create Layout.js

---
+++ Layout.js
@@ -0,0 +1,11 @@
+// components/Layout.js
+import Navbar from "./Navbar";
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Navbar />
+      <main>{children}</main>
+    </div>
+  );
+}
