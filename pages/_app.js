adfefa4fda749e8d13e9f407cc5ec7128b1138df
nahman1972 <hristov.dumitru@gmail.com>
2025-06-08 04:29:17 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
-// pages/_app.js
-import '../styles/globals.css';
+import '../styles/globals.css'
+import Layout from '../Layout'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
