84cc1ba4424ff05935ef24d7c23b03167497b2f1
nahman1972 <hristov.dumitru@gmail.com>
2025-06-08 03:28:58 -0400

    Update Layout.js

--- Layout.js
+++ Layout.js
@@ -1,11 +1,19 @@
 // components/Layout.js
+import Head from "next/head";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <Navbar />
-      <main>{children}</main>
-    </div>
+    <>
+      <Head>
+        <title>LostChain</title>
+        <meta name="description" content="Recover your lost crypto assets easily with LostChain." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div>
+        <Navbar />
+        <main>{children}</main>
+      </div>
+    </>
   );
 }
