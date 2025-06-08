2d7472b7c4d42100de2d75422f718f018a43d565
nahman1972 <hristov.dumitru@gmail.com>
2025-06-08 03:03:11 -0400

    Create support.js

---
+++ support.js
@@ -0,0 +1,8 @@
+export default function SupportPage() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Support</h1>
+      <p>If you need help, email us at <strong>support@lostchain.app</strong></p>
+    </div>
+  );
+}
