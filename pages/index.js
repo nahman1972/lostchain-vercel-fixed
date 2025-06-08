e1adf958e61cbe7d9d0d776697ab5af4cdc402e1
nahman1972 <hristov.dumitru@gmail.com>
2025-06-08 00:58:43 -0400

    Create pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ display: 'grid', placeItems: 'center', height: '100vh' }}>
+      <h1>✅ LostChain is live!</h1>
+      <p>Welcome to the crypto recovery app.</p>
+    </div>
+  );
+}
